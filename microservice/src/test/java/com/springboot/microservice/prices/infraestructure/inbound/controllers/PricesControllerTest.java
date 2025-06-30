64314565874b8f8c792e4ac434788d10892a1c90
IsmaelRP <37997622+IsmaelRP@users.noreply.github.com>
2025-06-30 11:39:12 +0200

    test(refactor): refactored test
    - Refactored tests
    - Added more test-cases
    - Replaced deprecated MockBean for MockitoBean

--- microservice/src/test/java/com/springboot/microservice/prices/infraestructure/inbound/controllers/PricesControllerTest.java
+++ microservice/src/test/java/com/springboot/microservice/prices/infraestructure/inbound/controllers/PricesControllerTest.java
@@ -1,76 +1,85 @@
 package com.springboot.microservice.prices.infraestructure.inbound.controllers;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.time.LocalDateTime;
-import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.slf4j.Logger;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
 
 import com.springboot.microservice.prices.PriceQueryTestData;
 import com.springboot.microservice.prices.application.obtainPrice.ObtainPriceUseCase;
+import com.springboot.microservice.prices.domain.exceptions.PriceNotFoundException;
 import com.springboot.microservice.prices.domain.model.PriceQuery;
-import com.springboot.microservice.shared.ResponseObj;
 
-@ExtendWith(MockitoExtension.class)
-@SpringBootTest
+
+@WebMvcTest(controllers = PricesController.class, excludeAutoConfiguration = SecurityAutoConfiguration.class)
 class PricesControllerTest {
-
-    @Mock
+	
+	@Autowired
+    private MockMvc mockMvc;
+	
+	@MockitoBean
     private ObtainPriceUseCase pricesUseCase;
 
-    @Mock
-    private Logger logger;
 
-    @InjectMocks
-    private PricesController pricesController;
-
-
+    private final PriceQuery priceQuery = PriceQueryTestData.samplePriceQuery();
+    
     @Test
-    void findPrice_PriceFound_ReturnsOkResponse() {
-        PriceQuery priceQuery = PriceQueryTestData.samplePriceQuery();
+    void findPrice_PriceFound_ReturnsOkResponse() throws Exception {
         when(pricesUseCase.findPriceByDate(
-        		priceQuery.startDate(),
-				priceQuery.productId(), 
-				priceQuery.brandId()
-			)).thenReturn(Optional.of(priceQuery));
+                priceQuery.startDate(),
+                priceQuery.productId(),
+                priceQuery.brandId()
+        )).thenReturn(priceQuery);
 
-        @SuppressWarnings("rawtypes")
-		ResponseEntity<ResponseObj> responseEntity = pricesController.findPrice(
-				priceQuery.brandId(), 
-				priceQuery.productId(), 
-				priceQuery.startDate());
-
-        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-        assertEquals("Price found", responseEntity.getBody().getMsg());
-        assertEquals(priceQuery, responseEntity.getBody().getBody());
-        verify(logger).info("Price controller - findPrice successful");
+        mockMvc.perform(get("/prices/findPrice")
+                .param("subsidiary_id", String.valueOf(priceQuery.brandId()))
+                .param("product_id", String.valueOf(priceQuery.productId()))
+                .param("application", priceQuery.startDate().toString())
+                .contentType(MediaType.APPLICATION_JSON))
+        .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.msg").value("Price found"))
+            .andExpect(jsonPath("$.body.productId").value(priceQuery.productId()))
+            .andExpect(jsonPath("$.body.brandId").value((int) priceQuery.brandId()))
+            .andExpect(jsonPath("$.body.price").value((double) priceQuery.price()));
     }
+
 
     @Test
-    void findPrice_PriceNotFound_ReturnsNotFoundResponse() {
-        short subsidiaryId = 1;
-        int productId = 123;
-        LocalDateTime application = LocalDateTime.now();
-        when(pricesUseCase.findPriceByDate(application, productId, subsidiaryId))
-                .thenReturn(Optional.empty());
+    void findPrice_PriceNotFound_ReturnsNotFoundResponse() throws Exception {
+        int nonProductId = 9999;
+        short brandId = 1;
+        String application = "2020-06-14T10:00:00";
 
-        @SuppressWarnings("rawtypes")
-		ResponseEntity<ResponseObj> responseEntity = pricesController.findPrice(subsidiaryId, productId, application);
+        when(pricesUseCase.findPriceByDate(
+                LocalDateTime.parse(application),
+                nonProductId,
+                brandId
+        )).thenThrow(new PriceNotFoundException("Price not found"));
 
-        assertEquals(HttpStatus.NOT_FOUND, responseEntity.getStatusCode());
-        assertEquals("Price not found", responseEntity.getBody().getMsg());
-        assertEquals("Error", responseEntity.getBody().getBody());
-        verify(logger).warn("Price controller - findPrice unsuccessful");
+        mockMvc.perform(get("/prices/findPrice")
+                .param("subsidiary_id", String.valueOf(brandId))
+                .param("product_id", String.valueOf(nonProductId))
+                .param("application", application)
+                .contentType(MediaType.APPLICATION_JSON))
+        .andDo(print())
+            .andExpect(status().isNotFound())
+            .andExpect(jsonPath("$.msg").value("Price not found"))
+            .andExpect(jsonPath("$.body").value("Error"));
     }
+
+
+
+
 }
