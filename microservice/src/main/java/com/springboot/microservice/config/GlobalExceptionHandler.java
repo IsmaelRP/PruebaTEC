b7516f97fcbeea3392b8170123a97c36c3ff43b1
IsmaelRP <37997622+IsmaelRP@users.noreply.github.com>
2025-06-30 09:59:57 +0200

    fix(exceptions): added exception handler
    - Added a global exception handler to provide a custom error with required parameters

---
+++ microservice/src/main/java/com/springboot/microservice/config/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.springboot.microservice.config;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<Map<String, Object>> handleMissingParams(MissingServletRequestParameterException ex) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("error", "Missing parameter");
+        response.put("parameter", ex.getParameterName());
+        response.put("message", ex.getMessage());
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+}
