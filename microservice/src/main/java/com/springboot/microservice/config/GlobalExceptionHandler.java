c93bd1c24081adc80e2d136ef20b32ef057eb0ae
IsmaelRP <37997622+IsmaelRP@users.noreply.github.com>
2025-07-01 14:25:21 +0200

    fix(exception): unified errors
    - Unified error's format

--- microservice/src/main/java/com/springboot/microservice/config/GlobalExceptionHandler.java
+++ microservice/src/main/java/com/springboot/microservice/config/GlobalExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import com.springboot.microservice.prices.domain.exceptions.PriceNotFoundException;
 import com.springboot.microservice.shared.ResponseObj;
@@ -18,9 +19,8 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(MissingServletRequestParameterException.class)
     public ResponseEntity<Map<String, Object>> handleMissingParams(MissingServletRequestParameterException ex) {
         Map<String, Object> response = new HashMap<>();
-        response.put("error", "Missing parameter");
-        response.put("parameter", ex.getParameterName());
-        response.put("message", ex.getMessage());
+        response.put("body", "Error, missing parameter");
+        response.put("msg", ex.getMessage());
 
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
@@ -28,7 +28,17 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(PriceNotFoundException.class)
     public ResponseEntity<ResponseObj<String>> handlePriceNotFound(PriceNotFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(new ResponseObj<>("Error", ex.getMessage()));
+                .body(new ResponseObj<>("Error, price not found", ex.getMessage()));
     }
+    
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<Map<String, Object>> handleTypeMismatch(MethodArgumentTypeMismatchException ex) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("body", "Error, nvalid parameter type");
+        response.put("msg", ex.getMessage());
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+    
 
 }
